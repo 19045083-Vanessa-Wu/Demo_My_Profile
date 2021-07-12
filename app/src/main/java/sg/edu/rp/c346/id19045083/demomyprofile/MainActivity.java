9107f0c3dfa754bfa03cb07ecfc272388edeb604
19045083-Vanessa-Wu <19045083@myrp.edu.sg>
2021-07-12 10:30:56 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19045083/demomyprofile/MainActivity.java
@@ -0,0 +1,69 @@
+package sg.edu.rp.c346.id19045083.demomyprofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etName;
+    EditText etGPA;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.editTextName);
+        etGPA = findViewById(R.id.editTextGPA);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+
+
+        // Step 1a: Get the user input from the EditText and store it in a variable
+        String strName = etName.getText().toString();
+        String gpa = etGPA.getText().toString();
+        float fgpa = 0;
+        if (gpa.length()>0){
+            fgpa = Float.parseFloat(gpa);
+        }
+
+        // Step 1b: Obtain an instance of the SharedPreferences
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+
+        // Step 1c: Obtain an instance of the SharedPreferences Editor for update later
+        SharedPreferences.Editor prefEditName = prefs.edit();
+        SharedPreferences.Editor prefEditGPA = prefs.edit();
+
+        // Step 1d: Add the key-value pair
+        prefEditName.putString("Name", strName);
+        prefEditGPA.putFloat("GPA", fgpa);
+
+        // Step 1e: Call commit() to save the changes into SharedPreference
+        prefEditName.commit();
+        prefEditGPA.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Step 2a: Obtain an instance of the SharedPreferences
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+
+        // Step 2b: Retrieve the saved data from the SharedPreferences object
+        String SetName = prefs.getString("Name", "User");
+        Float SetGPA = prefs.getFloat("GPA", 0);
+
+        // Step 2c: Updated the UI element with the value
+        etName.setText(SetName);
+        etGPA.setText(SetGPA.toString());
+    }
+}
